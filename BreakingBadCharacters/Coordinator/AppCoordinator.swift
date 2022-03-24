98a42e0c19eecf862a90b29ee5d1b306e32e2198
Alberto García Antuña <garantalberto@gmail.com>
2022-03-24 19:27:44 +0100

    Added AppCoordinator for instantiate the navigation controller

---
+++ BreakingBadCharacters/Coordinator/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  BreakingBadCharacters
+//
+//  Created by Alberto García Antuña on 24/3/22.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+        
+    let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    override func start() {
+        let navigationController = UINavigationController()
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+
+    override func finish() {}
+}
--- BreakingBadCharacters/SceneDelegate.swift
+++ BreakingBadCharacters/SceneDelegate.swift
@@ -11,11 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    lazy var appCoordinator: AppCoordinator = {
+        guard let window = self.window else { fatalError() }
+        return AppCoordinator(window: window)
+    }()
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let sceneWindow = UIWindow(windowScene: windowScene)
+        window = sceneWindow
+        appCoordinator.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,9 +51,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        
     }
-
-
 }
-
