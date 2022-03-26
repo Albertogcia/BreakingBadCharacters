a4a5b9ca3a95e4610726b8faa356550ea3f6bee9
Alberto García Antuña <garantalberto@gmail.com>
2022-03-26 17:13:28 +0100

    Extract shared URLProtocolStub to his own file

--- BreakingBadCharactersTests/URLSessionTests.swift
+++ BreakingBadCharactersTests/URLSessionTests.swift
@@ -33,7 +33,7 @@ class URLSessionTests: XCTestCase {
 
         let exp = expectation(description: "Wait for request")
         sut.fetchAllCharacters { result in
-            switch result{
+            switch result {
             case let .success(characters):
                 if let characters = characters, !characters.isEmpty {
                     let responseCharacter = characters[0]
@@ -44,17 +44,17 @@ class URLSessionTests: XCTestCase {
                     XCTAssertEqual(responseCharacter.breakingBadSeasonsAppearance?.first, appearence)
                     XCTAssertEqual(responseCharacter.betterCallSaulSeasonsAppearance?.first, bcsAppearence)
                 }
-                else{
+                else {
                     XCTFail("Expected characters but instead got empty")
                 }
-                
+
             case .failure:
                 XCTFail("Expected success but instead got \(result)")
             }
             exp.fulfill()
         }
         URLProtocolStub.stub(data: successfulData, response: successfulHTTPURLResponse(), error: nil)
-        
+
         wait(for: [exp], timeout: 1.0)
     }
 
@@ -79,66 +79,5 @@ class URLSessionTests: XCTestCase {
         }]
         """
         return Data(json.utf8)
-    }
-
-    // MARK: - URLProtocolStub
-
-    private class URLProtocolStub: URLProtocol {
-        private static var stub: Stub?
-        private static var requestObserver: ((URLRequest) -> Void)?
-
-        private struct Stub {
-            let data: Data?
-            let response: URLResponse?
-            let error: Error?
-        }
-
-        static func stub(data: Data?, response: URLResponse?, error: Error?) {
-            stub = Stub(data: data, response: response, error: error)
-        }
-
-        static func observeRequest(observer: @escaping (URLRequest) -> Void) {
-            requestObserver = observer
-        }
-
-        static func startInterceptingRequests() {
-            URLProtocol.registerClass(URLProtocolStub.self)
-        }
-
-        static func stopInterceptingRequests() {
-            URLProtocol.unregisterClass(URLProtocolStub.self)
-            stub = nil
-            requestObserver = nil
-        }
-
-        override class func canInit(with request: URLRequest) -> Bool {
-            return true
-        }
-
-        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            return request
-        }
-
-        override func startLoading() {
-            if let requestObserver = URLProtocolStub.requestObserver {
-                client?.urlProtocolDidFinishLoading(self)
-                return requestObserver(request)
-            }
-            if let data = URLProtocolStub.stub?.data {
-                client?.urlProtocol(self, didLoad: data)
-            }
-
-            if let response = URLProtocolStub.stub?.response {
-                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-            }
-
-            if let error = URLProtocolStub.stub?.error {
-                client?.urlProtocol(self, didFailWithError: error)
-            }
-
-            client?.urlProtocolDidFinishLoading(self)
-        }
-
-        override func stopLoading() {}
     }
 }
