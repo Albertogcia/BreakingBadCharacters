40fe52bf7fb71682e8cf2a9706d93f031cdb575d
Alberto García Antuña <garantalberto@gmail.com>
2022-03-27 19:49:13 +0200

    Implementation of CharactersViewModel with fetch method and helpers

--- BreakingBadCharacters/Characters/CharactersViewModel.swift
+++ BreakingBadCharacters/Characters/CharactersViewModel.swift
@@ -7,20 +7,92 @@
 
 import Foundation
 
-protocol CharactersCoordinatorDelegate: AnyObject{
-    
+protocol CharactersCoordinatorDelegate: AnyObject {
+    func didSelect(character: Character)
 }
 
 protocol CharactersViewDelegate: AnyObject {
-    
+    func charactersFetched()
+    func errorFetchingCharacters(error: Error)
 }
 
-class CharactersViewModel{
+class CharactersViewModel {
     weak var coordinatorDelegate: CharactersCoordinatorDelegate?
     weak var viewDelgate: CharactersViewDelegate?
     let charactersDataManager: CharactersDataManager
-    
-    init(charactersDataManager: CharactersDataManager){
+
+    var breakingBadCharacters: [Character] = []
+    var betterCallSaulCharacters: [Character] = []
+
+    var hasData: Bool {
+        return !breakingBadCharacters.isEmpty || !betterCallSaulCharacters.isEmpty
+    }
+
+    init(charactersDataManager: CharactersDataManager) {
         self.charactersDataManager = charactersDataManager
+    }
+
+    func fetchAllCharacters() {
+        breakingBadCharacters = []
+        betterCallSaulCharacters = []
+        charactersDataManager.fetchAllCharacters { [weak self] result in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                switch result {
+                case let .success(characters):
+                    self.groupCharacters(characters: characters)
+                case let .failure(error):
+                    self.viewDelgate?.errorFetchingCharacters(error: error)
+                }
+            }
+        }
+    }
+
+    private func groupCharacters(characters: [Character]?) {
+        for character in characters ?? [] {
+            if !(character.breakingBadSeasonsAppearance ?? []).isEmpty {
+                breakingBadCharacters.append(character)
+            }
+            if !(character.betterCallSaulSeasonsAppearance ?? []).isEmpty {
+                betterCallSaulCharacters.append(character)
+            }
+        }
+        viewDelgate?.charactersFetched()
+    }
+
+    // MARK: - TableView helper methods
+
+    func numberOfSections() -> Int {
+        return 2
+    }
+
+    func numberOfRows(in section: Int) -> Int {
+        switch section {
+        case 0:
+            return breakingBadCharacters.count
+        case 1:
+            return betterCallSaulCharacters.count
+        default:
+            return 0
+        }
+    }
+
+    func character(at indexPath: IndexPath) -> Character? {
+        if indexPath.section == 0 {
+            return breakingBadCharacters[indexPath.row]
+        }
+        else if indexPath.section == 1 {
+            return betterCallSaulCharacters[indexPath.row]
+        }
+        return nil
+    }
+
+    func didSelectRow(at indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            coordinatorDelegate?.didSelect(character: breakingBadCharacters[indexPath.row])
+        }
+        else if indexPath.section == 1 {
+            coordinatorDelegate?.didSelect(character: betterCallSaulCharacters[indexPath.row])
+        }
     }
 }
